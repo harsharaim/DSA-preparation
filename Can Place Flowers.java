9b805fc9a52f22d455290f11d9380ab7810c09b4
Harsha Rai M <86067494+harsharaim@users.noreply.github.com>
2023-07-25 18:01:32 +0530

    Update Can Place Flowers.java

--- Can Place Flowers.java
+++ Can Place Flowers.java
@@ -1,3 +1,17 @@
+// 605. Can Place Flowers
+// Easy
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new 
+//flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+// Example 1:
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+
+// Example 2:
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         if(n==0) return true; // if n=0 answer always true
