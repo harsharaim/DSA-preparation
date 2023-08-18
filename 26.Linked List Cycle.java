9bc10b271af5eca424cae51a8943ab69d4ea6524
Harsha Rai M <86067494+harsharaim@users.noreply.github.com>
2023-08-18 21:46:05 +0530

    Update 26.Linked List Cycle.java

--- 26.Linked List Cycle.java
+++ 26.Linked List Cycle.java
@@ -38,7 +38,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast=head;
         ListNode slow=head;
-        while(slow!=null && fast!=null && fast.next!=null){
+        while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
             if(fast==slow) return true;
